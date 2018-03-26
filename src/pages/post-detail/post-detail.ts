c50047e2455a3ddb181322b1a4148457b5da40de
MyNameIsSL <slim81@asdasdasdasdsdslim81.com>
2018-03-26 18:07:34 +1000

    Added b64 seed data

--- src/pages/post-detail/post-detail.ts
+++ src/pages/post-detail/post-detail.ts
@@ -1,5 +1,8 @@
+import { Phost } from './../../providers/photo-server-handler/photo-server-handler';
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DataCallback } from '../home/home';
 
 /**
  * Generated class for the PostDetailPage page.
@@ -14,12 +17,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'post-detail.html',
 })
 export class PostDetailPage {
-
+  data: Phost;
+  masterCallback: DataCallback;
+  keys; 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PostDetailPage');
+    this.loadDetailsData();
+  }
+  loadDetailsData() {
+    this.data = this.navParams.get('data') || [];
+    this.masterCallback = this.navParams.get('callback');
+    this.keys = Object.keys(this.data)
   }
 
+  // Submit data and pop back to the previous page.
+  submitDetailsToPageCallback() {
+    this.masterCallback(this.data)
+      .then((res) => {
+        console.log(":PostDetail: Submitted details data", this.data, res);
+        this.navCtrl.pop();
+      });
+  }
+  pass() {
+    console.log("pass");
+  }
 }
